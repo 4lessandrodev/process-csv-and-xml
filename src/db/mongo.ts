402d2e6745690833437b9894428c0594450c2959
4lessandrodev <alessandroadm@live.com>
2022-04-03 18:23:27 -0300

    chore(mongo): set env vars for mongo connection

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -5,16 +5,20 @@ export const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 export const MONGO_DB_HOST = process.env.MONGO_DB_HOST;
 export const MONGO_DB_NAME = process.env.MONGO_DB_NAME ?? 'test';
 export const MONGO_DB_COLLECTION = process.env.MONGO_DB_COLLECTION ?? 'transactions';
+export const MONGO_DB_PREFIX = process.env.MONGO_DB_PREFIX ?? 'mongodb:';
+export const MONGO_DB_PARAMS = MONGO_DB_PREFIX === 'mongodb:+srv' ? process.env.MONGO_DB_PARAMS : '';
 
 export class Mongo {
 	protected static conn: MongoClient | null = null;
+
+	protected static readonly URI = `${MONGO_DB_PREFIX}//${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_NAME}?${MONGO_DB_PARAMS}`;
 	
 	public static async init(): Promise<MongoClient> {
 		console.log('connecting to mongodb ...');
 		
 		if (!Mongo.conn) {
 			
-			const client = new MongoClient(`mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_NAME}?ssl=true&authSource=admin&w=majority`, {
+			const client = new MongoClient(Mongo.URI, {
 				rejectUnauthorized: false,
 				loggerLevel: 'debug',
 				keepAlive: true
